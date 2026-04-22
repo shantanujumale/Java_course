8ffb7c2b2ab365271903987352a50bcf8e4a89ef
Shantanu Jumale <shantanujumale92@gmail.com>
2026-04-22 09:30:45 +0530

    Add files via upload

---
+++ Collection_Framework/HashMapExample1.java
@@ -0,0 +1,11 @@
+package collecttion.framework;
+
+public class HashMapExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> data = new 
+
+	}
+
+}

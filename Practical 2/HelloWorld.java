a2c64d9a4f7502c9bf14c22463ef4deba0995a65
Shantanu Jumale <shantanujumale92@gmail.com>
2026-04-23 20:28:55 +0530

    Add files via upload

---
+++ Practical 2/HelloWorld.java
@@ -0,0 +1,10 @@
+package practice.JAVA;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!");
+		System.out.println("Arjun Balpande");
+	}
+
+}

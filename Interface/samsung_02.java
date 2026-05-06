3f3ea70173c1616ee0cbd12c647ff07d7cc08c4d
Shantanu Jumale <shantanujumale92@gmail.com>
2026-05-06 23:49:15 +0530

    Implement Samsung class with Radio and Camera interfaces

---
+++ Interface/samsung_02.java
@@ -0,0 +1,32 @@
+
+public class Samsung implements Radio, Camera{
+
+	@Override
+	public void clickPhoto() {
+		// TODO Auto-generated method stub
+		System.out.println("Clicking Image...");
+		
+	}
+
+	@Override
+	public void showPhoto() {
+		// TODO Auto-generated method stub
+		System.out.println("Showing Image...");
+		
+	}
+
+	@Override
+	public void scanStation() {
+		// TODO Auto-generated method stub
+		System.out.println("Scanning station...");
+		
+	}
+
+	@Override
+	public void playStation() {
+		// TODO Auto-generated method stub
+		System.out.println("Playing station...");
+		
+	}
+
+}
